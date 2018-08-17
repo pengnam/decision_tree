027f47e833ab96c3f1e0581f5d16dae618ec22ba
pengnam <seanngpengnam@gmail.com>
2018-08-17 23:48:06 +0800

    feat(rough models)

---
+++ model.py
@@ -0,0 +1,17 @@
+class Decision_Node:
+    def __init__(self, question, true_branch, false_branch):
+        self.question = question
+        self.true_branch = true_branch
+        self.false_branch = false_branch
+
+    def build_tree(rows):
+
+    def print_tree(node, spacing=""):
+
+    def classify(row, node):
+
+    def print_leaf(counts):
+
+def Leaf:
+    def __init__(self, rows):
+
