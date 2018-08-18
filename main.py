7ff204ac9c4103bafc2210393d1015c060bf12de
pengnam <seanngpengnam@gmail.com>
2018-08-18 17:49:16 +0800

    feat(main.py)

---
+++ main.py
@@ -0,0 +1,32 @@
+from utils import (print_tree, print_leaf, build_tree)
+# Adapted from https://github.com/random-forests/tutorials/blob/master/decision_tree.py
+training_data = [
+    ['Green', 3, 'Apple'],
+    ['Yellow', 3, 'Apple'],
+    ['Red', 1, 'Grape'],
+    ['Red', 1, 'Grape'],
+    ['Yellow', 3, 'Lemon'],
+]
+
+# Column labels.
+# These are used only to print the tree.
+header = ["color", "diameter", "label"]
+
+my_tree = build_tree(training_data)
+
+
+print_tree(my_tree)
+
+# Evaluate
+testing_data = [
+    ['Green', 3, 'Apple'],
+    ['Yellow', 4, 'Apple'],
+    ['Red', 2, 'Grape'],
+    ['Red', 1, 'Grape'],
+    ['Yellow', 3, 'Lemon'],
+]
+
+for row in testing_data:
+    print ("Actual: %s. Predicted: %s" %
+           (row[-1], print_leaf(my_tree.classify(row))))
+
